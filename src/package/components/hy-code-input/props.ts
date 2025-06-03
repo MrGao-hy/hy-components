7db6832a6f3f963a045bf4640018ac5a60210dae
gaoxh <2036902768@qq.com>
2025-06-03 14:53:40 +0800

    feat: 新增验证码输入框

---
+++ src/package/components/hy-code-input/props.ts
@@ -0,0 +1,21 @@
+import type IProps from "./typing";
+
+const defaultProps: IProps = {
+  modelValue: "",
+  adjustPosition: true,
+  maxlength: 6,
+  dot: false,
+  mode: "box",
+  hairline: false,
+  space: 10,
+  focus: false,
+  bold: false,
+  color: "",
+  fontSize: 18,
+  size: 35,
+  disabledKeyboard: false,
+  borderColor: "",
+  disabledDot: true,
+};
+
+export default defaultProps;
